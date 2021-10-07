ef0c9dfd229f751080f7bb762c50732479873629
Atique <ateeq@ipscloud.co>
2021-10-07 12:15:56 +0500

    Changes in Appointment Modal

--- resources/views/appointment_book/partials/_item_Service.blade.php
+++ resources/views/appointment_book/partials/_item_Service.blade.php
@@ -1,13 +1,13 @@
 <div class="deleteRow">
 	<div class="card">
+		<h5 class="card-title card-heading mb-0">
+        	<a href="#" onclick="deleteRow()">  {{ $getData->name ?? '' }} <i class="fas fa-times float-end close-btn"></i>
+        	</a>
+	     </h5>
 	    <div class="card-body"> 
-	        <h5 class="card-title mb-4">
-	        	<a href="#" onclick="deleteRow()">{{ $getData->name ?? '' }} <i class="fas fa-window-close"></i>
-	        	</a>
-	        </h5>
 	        <input type="hidden" name="services[]" value="{{ $getData->id }}">
 	         <div class="row mb-3">
-	            <div class="col-lg-6">
+	            <div class="col-lg-4">
 	                <label
 	                        class="form-label">Time Start </label>
 	                <div class="input-group" id="timepicker-input-group1">
@@ -18,9 +18,7 @@
 	                    <span class="input-group-text"><i class="mdi mdi-clock-outline"></i></span>
 	                </div>
 	            </div>
-	        </div>
-	        <div class="row mb-3">
-	            <div class="col-lg-6">
+	            <div class="col-lg-4">
 	                <label
 	                        class="form-label">Time Duration </label>
 	                <div class="input-group" id="timepicker-input-group1">
@@ -29,11 +27,7 @@
 	                    <span class="input-group-text"><i class="mdi mdi-clock-outline"></i></span>
 	                </div>
 	            </div>
-	        </div>
-
-
-	        <div class="row">
-	            <div class="mb-3">
+	            <div class="col-lg-4">
 	                <label for="when" class="form-label required">Employee Type</label>
 	                <select id="select_client_drop_down" name="employee_type_id[{{ $getData->id }}]" class="form-control select2 schedule_details_modal_submit">
 	                    <option value="">Select</option>
@@ -42,21 +36,17 @@
 	                    @endforeach
 	                </select>
 	            </div>
-	        </div>                        
-
+	        </div>                
 	        <div class="row">
-	            <div class="mb-3">
+	            <div class="col-lg-6">
 	                <label for="when" class="form-label required">Quantity</label>
 	                <input class="form-control schedule_details_modal_submit" name="quantity[{{ $getData->id }}]" value="1" type="text" placeholder="Quantity">
 	            </div>
-	        </div>
-	         <div class="row">
-	            <div class="mb-3">
+	            <div class="col-lg-6">
 	                <label for="when" class="form-label required">Price</label>
 	                <input class="form-control schedule_details_modal_submit" name="price[{{ $getData->id }}]" value="{{ $getData->price }}" type="text" placeholder="Price">
 	            </div>
 	        </div>
-
 	    </div>
 	    <!-- end card body -->
 	</div>
