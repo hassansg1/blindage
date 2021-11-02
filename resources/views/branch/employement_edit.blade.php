d6d54297d9fbc444c1deed2034d28d43d0332107
Atique <ateeq@ipscloud.co>
2021-11-02 22:42:49 +0500

    Atique

--- resources/views/branch/employement_edit.blade.php
+++ resources/views/branch/employement_edit.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-lg-12">
-        <form method="post" enctype="multipart/form-data" action="{{ route($route.'.emp_update',$item->id) }}">
+        <form class="needs-validation" novalidate method="post" enctype="multipart/form-data" action="{{ route($route.'.emp_update',$item->id) }}">
             @method('put')
             @include('branch.partials.employement_setup')
         </form>
