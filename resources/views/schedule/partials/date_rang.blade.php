d5d1b874106a3a044eb2a701be53df6959523278
akhtarnawaz <akhtarnawaz65@gmail.com>
2021-11-30 22:39:21 +0500

    reason

--- resources/views/schedule/partials/date_rang.blade.php
+++ resources/views/schedule/partials/date_rang.blade.php
@@ -56,12 +56,16 @@
     </td>
 
     @foreach ($period as $key => $value)
-
-        <td class="businessHoursOpen @if(getStartTime($value->format('Y-m-d')) == 1) available @endif">
+    @php($branchData = checkBranchOpen($value->format('Y-m-d')))
+        <td class="businessHoursOpen @if(isset($branchData->is_open)?$branchData->is_open:'0' == 1) available @endif">
         <a class="available_wrapper" href="#" onclick="getModalData('{{$value->format('Y-m-d')}}','{{$key}}')"
         >
             <div class="timing" id="{{$value->format('Ymd')}}">
+                @if(isset($branchData->is_open)?$branchData->is_open:'0' == 1)
                 {!! getBranchTime($value->format('Y-m-d')) !!}
+                @else
+                    {{isset($branchData->reason)?$branchData->reason:'Off Day'}}
+                @endif
             </div>
         </a>
     </td>
